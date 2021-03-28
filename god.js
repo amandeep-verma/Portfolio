6a8240d405849c4c08e930e89160f6bc262df5e4
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2021-03-28 06:52:12 -0400

    Add files via upload

---
+++ god.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+                var i      = {};
+                i.am       = 'a software developer'; 
+                i.love     = ['dynamic coding', 'innovative ideas', 'poetry', 'sketches', 'digital art'];
+                i.workWith = ['java', 'Spring', 'python', 'R', 'AWS', 'JS', 'open-minded people'];
+                i.aspire   = ['to learn', 'to teach', 'to develop amazing products'];
